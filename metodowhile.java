21091320487ce0245cf385ef9655082e8b9bb099
Marco Junior <juninhomarco-@hotmail.com>
2023-08-28 14:05:19 -0300

    exercico28/08

---
+++ metodowhile.java
@@ -0,0 +1,10 @@
+public class metodowhile {
+    
+    public static void main(String[] args) {
+		
+        int contador = 1;
+        while(contador <= 10){ //while serve pra repetir varias vezes.
+            System.out.printf("i = %d\n", contador);
+            contador ++;}
+        }
+}
